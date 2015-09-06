30e693e52cd3a54f0bd37fdab111e69c8b00baa1
Wojciech Lichota <wojciech@lichota.pl>
2015-09-06 11:45:19 +0000

    full quizer functionality with tests

--- quizer_tests.py
+++ quizer_tests.py
@@ -16,6 +16,7 @@ class QuizerTestCase(unittest.TestCase):
         Setup environment before each unittest.
         """
         quizer.app.config['TESTING'] = True
+        quizer.app.config['DATA_FILE'] = 'data/test.csv'
         self.client = quizer.app.test_client()
 
     def tearDown(self):
@@ -60,15 +61,51 @@ class QuizerTestCase(unittest.TestCase):
         """
         Checks question page.
         """
-        resp = self.client.get('/pytanie')
+        client = quizer.app.test_client()
+        resp = client.post('/', data={'username': 'TEST'})
+        resp = client.get('/pytanie')
         self.assertEqual(resp.status_code, 200)
 
-    def test_result_page(self):
+        resp = client.post('/pytanie', data={'answer': 'A'})
+        self.assertEqual(resp.status_code, 302)
+        self.assertTrue(resp.headers['Location'].endswith('/pytanie'))
+
+    def test_no_answers(self):
         """
-        Checks result page.
+        Checks result calculation when all ansers are empty.
         """
+        resp = self.client.post('/', data={'username': 'TEST'})
+        for i in range(6):
+            resp = self.client.get('/pytanie')
+            if i < 5:
+                self.assertEqual(resp.status_code, 200)
+            else:
+                self.assertEqual(resp.status_code, 302)
+                self.assertTrue(resp.headers['Location'].endswith('/wynik'))
+
         resp = self.client.get('/wynik')
         self.assertEqual(resp.status_code, 200)
+        self.assertIn('wynik to: 0/15 (0%)', resp.data)
+
+    def test_max_points(self):
+        """
+        Checks result calculation when all ansers are correct.
+        """
+        resp = self.client.post('/', data={'username': 'TEST'})
+        for i in range(5):
+            resp = self.client.get('/pytanie')
+            self.assertEqual(resp.status_code, 200)
+            resp = self.client.post('/pytanie', data={'answer': 'A'})
+            self.assertEqual(resp.status_code, 302)
+
+            if i < 4:
+                self.assertTrue(resp.headers['Location'].endswith('/pytanie'))
+            else:
+                self.assertTrue(resp.headers['Location'].endswith('/wynik'))
+
+        resp = self.client.get('/wynik')
+        self.assertEqual(resp.status_code, 200)
+        self.assertIn('wynik to: 15/15 (100%)', resp.data)
 
 
 if __name__ == '__main__':
